068de8e5ca683811111c721b3664cf00955fe423
ErikYoung89 <erik.young.texas@gmail.com>
2023-11-13 15:15:39 -0600

    adding Rustlings solved exercises

---
+++ exercises/options/options2.rs
@@ -0,0 +1,50 @@
+// options2.rs
+// Execute `rustlings hint options2` or use the `hint` watch subcommand for a hint.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn simple_option() {
+        let target = "rustlings";
+        let optional_target = Some(target);
+
+        // TODO: Make this an if let statement whose value is "Some" type
+        if let Some(word) = optional_target {
+            assert_eq!(word, target);
+        }
+    }
+
+    #[test]
+    fn layered_option() {
+        let range = 10;
+        let mut optional_integers: Vec<Option<i8>> = vec![None];
+
+        for i in 1..(range + 1) {
+            optional_integers.push(Some(i));
+        }
+
+        let mut cursor = range;
+        println!("{}", range);
+
+        // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
+        // You can stack `Option<T>`s into while let and if let
+
+        // let mut counter = Some(0);
+        //
+        // while let Some(i) = counter {
+        //     if i == 10 {
+        //         counter = None;
+        //     } else {
+        //         println!("{i}");
+        //         counter = Some (i + 1);
+        //     }
+        //}
+       while let Some(Some(integer)) = optional_integers.pop() {
+            // optional_integers.pop returns Option<Option<i8>>
+            assert_eq!(integer, cursor);
+            cursor -= 1;
+        }
+
+        assert_eq!(cursor, 0);
+    }
+}
---
+++ exercises/traits/traits2.rs
@@ -0,0 +1,44 @@
+// traits2.rs
+//
+// Your task is to implement the trait
+// `AppendBar` for a vector of strings.
+//
+// To implement this trait, consider for
+// a moment what it means to 'append "Bar"'
+// to a vector of strings.
+//
+// No boiler plate code this time,
+// you can do this!
+// Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
+
+
+trait AppendBar {
+    fn append_bar(self) -> Self;
+}
+
+// TODO: Implement trait `AppendBar` for a vector of strings.
+impl AppendBar for Vec<String> {
+    // TODO: Implement `AppendBar` for type `String`.
+    fn append_bar(mut self) -> Self{
+        self.push("Bar".to_string());
+        self
+
+        // for x in self.iter() {
+        //     self.push("Bar");
+        // }
+        // format!("{self}Bar")
+        // self.push_str("Bar");
+        // self
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_vec_pop_eq_bar() {
+        let mut foo = vec![String::from("Foo")].append_bar();
+        assert_eq!(foo.pop().unwrap(), String::from("Bar"));
+        assert_eq!(foo.pop().unwrap(), String::from("Foo"));
+    }
+}
